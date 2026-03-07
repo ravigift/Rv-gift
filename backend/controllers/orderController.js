1f01ceb9cc8d1999b29b8fe425f4b80bbb74a3fb
Dhananjay Pandey <pandeydhananjay1444@gmail.com>
2026-03-07 17:35:29 +0530

    fix: email timeout + cancel order fix 2

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -11,13 +11,9 @@ import { adminOrderEmailHTML } from "../utils/adminOrderEmail.js";
    📧 EMAIL CONFIG
 ========================= */
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    connectionTimeout: 5000,
-    greetingTimeout: 5000,
-    socketTimeout: 10000,
+    host: "74.125.133.108", // ✅ smtp.gmail.com ka IPv4 address directly
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -329,3 +325,6 @@ export const getAllOrders = async (req, res) => {
         res.status(500).json({ message: "Failed to fetch all orders" });
     }
 };
+
+
+// wlco tkuz tplw cdnd
